5c9c638b879b48f7286dcc1582b1f50908bc06fb
nchakicherla <nchaki6@icloud.com>
2026-08-16 22:13:48 -0700

    main now test reads sample.csv; comment out repl

--- src/main.c
+++ src/main.c
@@ -10,23 +10,40 @@
 
 int main(void) {
 
-	char *line;
+	// char *line;
 
-	while ((line = linenoise("> ")) != NULL) {
-		linenoiseHistoryAdd(line);
+	// while ((line = linenoise("> ")) != NULL) {
+	// 	linenoiseHistoryAdd(line);
 
-		if (0 == strcmp(line, ".exit")) {
-			linenoiseFree(line);
-			break;
-		}
+	// 	if (0 == strcmp(line, ".exit")) {
+	// 		linenoiseFree(line);
+	// 		break;
+	// 	}
 
-		size_t line_len = strlen(line);
+	// 	size_t line_len = strlen(line);
 
-		printf("you entered: %s\n", line);
-		printf("line_len: %zu\n", line_len);
+	// 	printf("you entered: %s\n", line);
+	// 	printf("line_len: %zu\n", line_len);
 
-		linenoiseFree(line);
+	// 	linenoiseFree(line);
+	// }
+
+	Arena arena;
+	arena_init(&arena, 256);
+
+	const char *csv_path = "./resources/sample.csv";
+
+	size_t csv_len;
+	char *csv_buffer = file_read_all(&arena, csv_path, &csv_len);
+	if (!csv_buffer) {
+		fprintf(stderr, "failed to read csv: %s\n", csv_path);
 	}
+
+	if (csv_buffer) {
+		printf("%s\n", csv_buffer);
+	}
+
+	arena_destroy(&arena);
 
 	return 0;
 }
