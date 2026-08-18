ac533604a18ceb5ff8bf6386b7aaba11c6815421
nchakicherla <nchaki6@icloud.com>
2026-08-17 20:46:20 -0700

    add FieldRowCounter which accumulates number of cols (specific to header too if present) and rows in CSV, also accumulates number of bytes of data for each column

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "linenoise.h"
 #include "csv.h"
@@ -9,14 +10,46 @@
 #include "arena.h"
 #include "file.h"
 
+typedef struct FieldRowCounter {
+	size_t num_cols;
+	size_t num_rows;
+	size_t num_cols_header;
+	size_t num_cols_first_row; // only used if header row doesn't exist
+	bool header_row_exists;
+	bool error_encountered;
+	size_t total_col_length[64];
+} FieldRowCounter;
+
 static void cb1(void *field, size_t len, void *data) {
-	(void)data;
+	FieldRowCounter *counter = (FieldRowCounter *)data;
+	if (counter->header_row_exists && counter->num_rows == 0) {
+		counter->num_cols_header++;
+		counter->num_cols_first_row++;
+	}
+	if (!counter->header_row_exists && counter->num_rows == 0) {
+		counter->num_cols_first_row++;
+	}
+	if (counter->header_row_exists && counter->num_rows > 0) {
+		counter->total_col_length[counter->num_cols] += len + 1;
+	}
+	if (!counter->header_row_exists) {
+		counter->total_col_length[counter->num_cols] += len + 1;		
+	}
+	counter->num_cols++;
 	printf("field: %.*s\n", (int)len, (char *)field);
 }
 
 static void cb2(int c, void *data) {
 	(void)c;
-	(void)data;
+	
+	FieldRowCounter *counter = (FieldRowCounter *)data;
+	counter->num_rows++;
+
+	if (counter->header_row_exists && counter->num_cols_header != counter->num_cols) {
+		counter->error_encountered = true;
+	}
+	counter->num_cols = 0;
+
 	printf("row completed\n");
 }
 
@@ -43,7 +76,7 @@ int main(void) {
 	Arena arena;
 	arena_init(&arena, 256);
 
-	const char *csv_path = "./resources/sample_messy.csv";
+	const char *csv_path = "./resources/sample.csv";
 
 	size_t csv_len;
 	char *csv_buffer = file_read_all(&arena, csv_path, &csv_len);
@@ -65,7 +98,10 @@ int main(void) {
 		return 2;
 	}
 
-	size_t parsed = csv_parse(&parser, csv_buffer, csv_len, cb1, cb2, NULL);
+	FieldRowCounter counter = {0};
+	counter.header_row_exists = true;
+
+	size_t parsed = csv_parse(&parser, csv_buffer, csv_len, cb1, cb2, &counter);
 	if (parsed != csv_len) {
 		fprintf(stderr, "csv parse error: (message) %s\n", csv_strerror(csv_error(&parser)));
 		csv_free(&parser);
@@ -73,11 +109,20 @@ int main(void) {
 		return 3;
 	}
 
-	if (csv_fini(&parser, cb1, cb2, NULL) != 0) {
+	if (csv_fini(&parser, cb1, cb2, &counter) != 0) {
 		fprintf(stderr, "csv parse error: (message ) %s\n", csv_strerror(csv_error(&parser)));
 		csv_free(&parser);
 		arena_destroy(&arena);
 		return 4;
+	}
+
+	printf("counter.num_rows: %zu\n", counter.num_rows);
+	printf("counter.num_cols_header: %zu\n", counter.num_cols_header);
+	printf("counter.num_cols: %zu\n", counter.num_cols);
+	printf("counter.error_encountered: %d\n", counter.error_encountered);
+
+	for (size_t i = 0; i < counter.num_cols_header; i++) {
+		printf("counter.total_col_length[%zu]: %zu\n", i, counter.total_col_length[i]);
 	}
 
 	csv_free(&parser);
